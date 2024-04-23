000147746f311e44471f7ed4bdb53ee3beeb52d2
ytadlaoui <usftadlaoui@gmail.com>
2024-04-23 16:41:17 +0200

    Ajout composent header dans le dossier src/app/header/

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent  implements OnInit {
+
+  constructor() { }
+imageUrl="assets/icon/Vector.png"
+imageUrl1="assets/icon/ri_search-line.png"
+  ngOnInit() {}
+
+}
