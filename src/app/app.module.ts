b0e4654932f9923e935448a913aca2ba91e076ba
ytadlaoui <usftadlaoui@gmail.com>
2024-04-30 18:28:13 +0200

    ajouter page details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import  {FormsModule}  from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 // import { Tab2PageRoutingModule } from './tab2/tab2-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -3,6 +3,9 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../header/header.component';
+import { HeaderModule } from '../header/header.module';
+import { ButtonModule } from '../button/button.module';
 @Component({
   selector: 'app-tab3',
   standalone: true,
@@ -10,11 +13,18 @@ import { FormsModule } from '@angular/forms';
     IonicModule,
     CommonModule,
     FormsModule,
+    HeaderModule,
+    ButtonModule
   ],
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  imagurl3="assets/icon/messi.png";
+  imagurl4="assets/icon/Group.png";
+  imagurl5="assets/icon/performance.png";
+  imagurl6="assets/icon/statique.png";
+  imagurl7="assets/icon/trophies.png";
 
   constructor() {}
 
