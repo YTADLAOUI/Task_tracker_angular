cf4d7b84bb33a0e17b9cdd5d14dbaee4c37d6f96
ytadlaoui <usftadlaoui@gmail.com>
2024-04-30 16:02:00 +0200

    ajouter page d'ajouter jouer avec details

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
-
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-tab3',
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
