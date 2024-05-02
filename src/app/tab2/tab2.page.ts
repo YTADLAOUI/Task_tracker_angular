efbf1e9de0e818e174e33d16d0a746fd09fbe64c
ytadlaoui <usftadlaoui@gmail.com>
2024-05-02 12:34:05 +0200

    add reactive form

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { IonHeader } from "@ionic/angular/standalone";
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ButtonModule } from '../button/button.module';
 
 
 // import { Tab2PageRoutingModule } from './tab2-routing.module';
@@ -16,12 +17,27 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
+    ReactiveFormsModule,
+    ButtonModule,
   ],
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  infoPlayer = new FormGroup({
+    name: new FormControl(''),
+    position: new FormControl(''),
+    clubName: new FormControl(''),
+    height: new FormControl(''),
+    weight: new FormControl(''),
+    buts: new FormControl(''),
+    matches: new FormControl(''),
+    assists: new FormControl(''),
+    age: new FormControl(''),
+  })
   constructor() {}
-
+  handelSubmit(){
+      console.log(this.infoPlayer.value);
+      
+  };
 }
