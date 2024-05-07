596300d5b24e1a6cec8869ea63c5eb85bd8191f4
ytadlaoui <usftadlaoui@gmail.com>
2024-05-07 15:07:58 +0200

    Update player.module.ts

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerComponent } from './player.component';
+import { PlayerService } from './player.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [PlayerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    RouterModule
   ],
-  exports: [PlayerComponent]
+  exports: [PlayerComponent],
+  providers:[PlayerService]
 })
 export class PlayerModule { }
