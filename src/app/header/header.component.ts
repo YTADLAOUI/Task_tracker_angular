676d5fae9a6bbbd48c4bc05cf4c4acf5b9ed69f3
ytadlaoui <usftadlaoui@gmail.com>
2024-05-07 15:07:58 +0200

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit, } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { getSearch } from '../features/search/search.actions';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +9,11 @@ import { Component, Input, OnInit, } from '@angular/core';
   
 })
 export class HeaderComponent  implements OnInit {
-  search='';
-
-  constructor() { }
+  SearchValue='';
+  Search$!: Observable<string>;
+  constructor(private store: Store<{ search:string  }>) {
+    this.Search$ = this.store.select('search');
+  }
 imageUrl="assets/icon/Vector.png"
 imageUrl1="assets/icon/ri_search-line.png"
   ngOnInit() {
@@ -17,6 +21,18 @@ imageUrl1="assets/icon/ri_search-line.png"
   }
   
   displayValue=()=>{
-    console.log(this.search)
+    console.log(this.SearchValue)
+    this.store.dispatch(getSearch({search:this.SearchValue}))
+
+
+
+    console.log(this.Search$,"searVVch");
+    this.Search$.subscribe(value=>{
+      if (value) { 
+        console.log(value, "Search value from store");
+      } else {
+        console.log("Search value is empty");
+      }
+    });
   }
 }
