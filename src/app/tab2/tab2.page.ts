e9be233b1fe3545a03164378f3fccdd998ed9578
ytadlaoui <usftadlaoui@gmail.com>
2024-05-07 15:07:59 +0200

    Update tab2.page.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader } from "@ionic/angular/standalone";
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { ButtonModule } from '../button/button.module';
+import {  HttpClientModule } from '@angular/common/http';
+import { Tab2Service } from './tab2.service';
 
-
-// import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @Component({
   selector: 'app-tab2',
@@ -16,14 +15,19 @@ import { ButtonModule } from '../button/button.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     ReactiveFormsModule,
     ButtonModule,
+    HttpClientModule
   ],
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  providers: [Tab2Service]
 })
 export class Tab2Page {
+  constructor(private imageService: Tab2Service) { }
+  imagePlayer: any;
+  imageProfile: any;
+  logo: any;
   infoPlayer = new FormGroup({
     name: new FormControl(''),
     position: new FormControl(''),
@@ -35,9 +39,33 @@ export class Tab2Page {
     assists: new FormControl(''),
     age: new FormControl(''),
   })
-  constructor() {}
+  
   handelSubmit(){
-      console.log(this.infoPlayer.value);
-      
+  const formData = new FormData();
+        formData.append('imagePlayer', this.imagePlayer);
+        formData.append('imageProfile', this.imageProfile);
+        formData.append('logo', this.logo);
+        formData.append('name', this.infoPlayer.get("name")?.value ?? '');
+        formData.append('position',this.infoPlayer.get("position")?.value ?? '');
+        formData.append('clubName', this.infoPlayer.get("clubName")?.value ?? '');
+        formData.append('height', this.infoPlayer.get("height")?.value ?? '');
+        formData.append('weight', this.infoPlayer.get("weight")?.value ?? '');
+        formData.append('buts', this.infoPlayer.get("buts")?.value ?? '' );
+        formData.append('matches', this.infoPlayer.get("matches")?.value ?? '' );
+        formData.append('assists', this.infoPlayer.get("assists")?.value ?? '' );
+        formData.append('age', this.infoPlayer.get("age")?.value ?? '');
+    
+      this.imageService.addPlayer(formData).subscribe((res)=>{
+        console.log(res);
+      })
   };
+  imageProfiles(event:any){
+    this.imageProfile = event.target.files[0];
+  }
+  imagePlayers(event:any){
+    this.imagePlayer = event.target.files[0];
+  }
+  logos(event:any){
+    this.logo = event.target.files[0];
+  }
 }
